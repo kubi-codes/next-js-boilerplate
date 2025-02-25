d298b64c467024fccbf022657117dbac5beb608d
kubil <bilkisismail07@gmail.com>
2025-02-25 23:43:58 +0700

    update

--- src/components/_pages/Initiatives/Detail/Food_Security_Sustainability_Center/_food_security_sustainability_center.jsx
+++ src/components/_pages/Initiatives/Detail/Food_Security_Sustainability_Center/_food_security_sustainability_center.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import _food_security_sustainability_center_headmaster from "./_food_security_sustainability_center.headmaster";
 import _food_security_sustainability_center_content_1 from "./_food_security_sustainability_center.content_1";
+import _food_security_sustainability_center_content_2 from "./_food_security_sustainability_center.content_2";
 
 function _food_security_sustainability_center() {
   return (
     <>
       <_food_security_sustainability_center_headmaster />
       <_food_security_sustainability_center_content_1 />
+      <_food_security_sustainability_center_content_2 />
     </>
   );
 }
