3891e0d4cbe7071547de113d63ee186ab54e737a
Bilkis Ismail <bilkisismail07@gmail.com>
2025-04-09 22:59:16 +0700

    update

--- src/components/_pages/Home/_home.content_1.jsx
+++ src/components/_pages/Home/_home.content_1.jsx
@@ -1,11 +1,33 @@
 import React from "react";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, useTheme } from "@mui/material";
 
 function _home_content_1() {
+  const theme = useTheme();
+  
   return (
-    <Box bgcolor={(props) => props.palette.primary.main} paddingY="40px" mb="20px">
-      <Container>
-        <Typography color="#fff" align="center" fontSize="22px" lineHeight="36px">
+    <Box
+      bgcolor={(props) => props.palette.primary.main}
+      paddingY="40px"
+      mb="20px"
+    >
+      <Container maxWidth="lg">
+        <Typography
+          color="#fff"
+          align="center"
+          fontSize={{
+            xs: "16px",
+            sm: "18px",
+            md: "20px",
+            lg: "22px",
+          }}
+          lineHeight={{
+            xs: "26px",
+            sm: "30px",
+            md: "34px",
+            lg: "36px",
+          }}
+          fontFamily={theme.typography.body1.fontFamily}
+        >
           “Tay Juhana Foundation (TJF) is a nonprofit organization dedicated to
           advocate the conversion and cultivation of suboptimal lands into
           productive lands, through the most environmentally, economically, and
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -29,57 +29,111 @@ const theme = createTheme({
   typography: {
     h1: {
       fontFamily: Panton.style.fontFamily,
-      fontSize: "64px",
+      fontSize: "4rem", // 64px
+      lineHeight: 1.2,
+      "@media (max-width:1440px)": {
+        fontSize: "3rem", // 56px
+        lineHeight: 1,
+      },
+      "@media (max-width:1200px)": {
+        fontSize: "3rem", // 48px
+      },
+      "@media (max-width:900px)": {
+        fontSize: "2.25rem", // 36px
+      },
       "@media (max-width:600px)": {
-        fontSize: "36px",
+        fontSize: "2rem",
         lineHeight: "42px",
       },
     },
     h2: {
       fontFamily: Panton.style.fontFamily,
-      "@media (max-width:600px)": {
-        fontSize: "36px",
-      },
+      fontSize: "3rem",
+      [`@media (max-width:900px)`]: { fontSize: "2.25rem" },
     },
     h3: {
       fontFamily: Panton.style.fontFamily,
-      fontSize: "42px",
-      "@media (max-width:600px)": {
-        fontSize: "32px",
-      },
+      fontSize: "2.625rem", // 42px
+      [`@media (max-width:900px)`]: { fontSize: "2rem" },
+      [`@media (max-width:600px)`]: { fontSize: "1.75rem" },
     },
-    h4: { fontFamily: Panton.style.fontFamily, fontSize: "36px" },
+    h4: {
+      fontFamily: Panton.style.fontFamily,
+      fontSize: "2.25rem",
+    },
     h5: {
       fontFamily: Panton.style.fontFamily,
-      fontSize: "24px",
-      lineHeight: "36px",
+      fontSize: "1.5rem",
+      lineHeight: "2.25rem",
     },
-    h6: { fontFamily: Panton.style.fontFamily, fontSize: "16px" },
+    h6: {
+      fontFamily: Panton.style.fontFamily,
+      fontSize: "1rem",
+    },
     body1: {
       fontFamily: Avenir_Roman.style.fontFamily,
-      "@media (max-width:600px)": {
-        fontSize: "14px",
-        lineHeight: "21px",
+      fontSize: "1rem",
+      lineHeight: "1.5rem",
+      [`@media (max-width:600px)`]: {
+        fontSize: "0.875rem",
+        lineHeight: "1.3125rem",
       },
     },
     body2: {
       fontFamily: Avenir_Roman.style.fontFamily,
+      fontSize: "0.875rem",
     },
   },
   components: {
+    MuiContainer: {
+      defaultProps: {
+        maxWidth: "xl", // defaultnya tetap xl
+      },
+      styleOverrides: {
+        root: {
+          paddingLeft: "1rem",
+          paddingRight: "1rem",
+          [`@media (min-width:600px)`]: {
+            paddingLeft: "2rem",
+            paddingRight: "2rem",
+          },
+
+          // MacBook 12 inch (<= 1280px)
+          [`@media (max-width:1280px)`]: {
+            maxWidth: "100%", // biar adaptif penuh
+            marginLeft: "auto",
+            marginRight: "auto",
+          },
+
+          // MacBook 13 inch (<= 1440px)
+          [`@media (max-width:1440px)`]: {
+            maxWidth: "960px", // ukuran 'md' supaya pas
+          },
+
+          // MacBook 14 inch (<= 1512px)
+          [`@media (max-width:1512px)`]: {
+            maxWidth: "1280px", // ukuran 'lg'
+          },
+
+          // MacBook 16 inch dan lebih (default)
+          [`@media (min-width:1728px)`]: {
+            maxWidth: "1440px", // tetap bisa dibatasi agar tidak terlalu melebar
+          },
+        },
+      },
+    },
     MuiButton: {
       defaultProps: { disableElevation: true },
       styleOverrides: {
         root: {
           fontFamily: Panton.style.fontFamily,
-          fontSize: "14px",
-          lineHeight: "19px",
-          padding: "18px 32px",
+          fontSize: "0.875rem",
+          lineHeight: "1.2rem",
+          padding: "1.125rem 2rem", // 18px 32px
           borderRadius: "8px",
           textTransform: "capitalize",
-
           "@media (max-width:600px)": {
-            padding: "10px 16px",
+            padding: "0.625rem 1rem", // 10px 16px
             borderRadius: "4px",
           },
         },
@@ -96,19 +150,14 @@ const theme = createTheme({
             borderRadius: "8px",
           },
           "& input": {
-            padding: "18px 20px",
-
+            padding: "1.125rem 1.25rem", // 18px 20px
+            fontSize: "1rem",
             "@media (max-width:600px)": {
-              padding: "10px 16px",
+              padding: "0.625rem 1rem", // 10px 16px
+              fontSize: "0.875rem",
             },
           },
-          "& .MuiInputBase-input": { fontSize: "16px" },
         },
-      },
-    },
-    MuiContainer: {
-      defaultProps: {
-        maxWidth: "xl",
       },
     },
   },
